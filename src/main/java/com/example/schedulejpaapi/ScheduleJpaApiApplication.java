eec2c76e23b1851f7f997767291893d30acc75b4
이준영 <tera199810@gmail.com>
2025-05-16 20:07:46 +0900

    Initial commit

---
+++ src/main/java/com/example/schedulejpaapi/ScheduleJpaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulejpaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJpaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJpaApiApplication.class, args);
+    }
+
+}
